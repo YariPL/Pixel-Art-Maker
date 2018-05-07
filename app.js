ee9c8199f34b04aa7c53643623743e309b7432a5
Yaroslav <yaroslav.khudchenko@gmail.com>
2018-05-07 12:13:52 +0200

    fix javascript file

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 "use strict";
 
+'use strict'
 function makeGrid() {
-	$('.gridForm').submit(function(e) {
-		e.preventDefault();
-		$('#pixels').html('');
+	$('.gridForm').submit(function(m) {
+		m.preventDefault(); //prevent page reload
+		$('#pixels').html(''); // prevent adding new cells to old
 		
 		let x=$('#gridHeight').val(); // value of height
 		let y=$('#gridWidth').val();  // value of width
 
-		for(i=0; x>i;x--){
-			$('#pixels').append('<tr><td></td></tr>');
+		for(let i=0; x>i;x--){
+			$('#pixels').append('<tr><td></td></tr>'); //create table - height
 		}
 
-		for(j=1; y>j ;y--){
-			$('tr').append('<td></td>');
+		for(let j=1; y>j ;y--){
+			$('tr').append('<td></td>');//create table - width
 		} 	
 	});
 }
 
 function draw(){  
-	$('table').on('click', "td", function() {
-		let $c=$('#colorCanvas').val();
-		$( this ).css({'background-color': $c });
+	$('table').on( "click", "td", function() {
+		let $c=$('#colorCanvas').val();//take color from picker
+		$( this ).css({'background-color': $c });//change td background
+	});
+}
+function erase(){  
+	$('table').on( "contextmenu", "td", function(e) {
+		e.preventDefault();
+		$( this ).css('background-color','white' ); // make td white on right clicks
 	});
 }
 
-	
-
 makeGrid();
 draw();
+erase();
